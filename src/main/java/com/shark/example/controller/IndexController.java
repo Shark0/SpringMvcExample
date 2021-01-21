4af422d848ec63b2406878225ae5fdb52569b60e
WK-15\Shark Lin <lin22223344@gmail.com>
2021-01-21 09:37:35 +0800

    jsp example

--- src/main/java/com/shark/example/controller/IndexController.java
+++ src/main/java/com/shark/example/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.shark.example.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/")
 public class IndexController {
 
+    @Value("${person.name}")
+    private String name;
+
     @GetMapping
     public ModelAndView index() {
         ModelAndView model = new ModelAndView("index");
+        model.addObject("name", name);
         return model;
     }
-
 }
--- src/main/java/com/shark/example/dao/mapper/CarMapper.java
+++
@@ -1,9 +0,0 @@
-package com.shark.example.dao.mapper;
-
-import com.shark.example.dao.pojo.CarDo;
-
-import java.util.List;
-
-public interface CarMapper {
-    public List<CarDo> findAll();
-}
