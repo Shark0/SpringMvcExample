c9475d263db960cb20252a3b186ea2400830b5a7
WK-15\Shark Lin <lin22223344@gmail.com>
2021-02-25 08:50:08 +0800

    新增i18n配置

--- src/main/java/com/shark/example/controller/IndexController.java
+++ src/main/java/com/shark/example/controller/IndexController.java
@@ -1,19 +1,27 @@
 package com.shark.example.controller;
 
 import com.shark.example.aop.TimeLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Locale;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/index")
 public class IndexController {
 
+    private final MessageSource messageSource;
+
     @TimeLog
     @GetMapping
     public HashMap read() {
-        return new HashMap();
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("message", messageSource.getMessage("hello.world", null, Locale.TAIWAN));
+        return hashMap;
     }
 }
